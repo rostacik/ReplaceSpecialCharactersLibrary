34e410d2de9cfd00767733b8aca3aebff06aabb1
dusan <rostacik@gmail.com>
2012-01-31 09:31:39 +0100

    initial commit - no code

---
+++ ReplaceSpecialCharactersLibrary/ReplaceSpecialCharactersLibrary/ReplaceSpecialCharacters.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ReplaceSpecialCharacters.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ReplaceSpecialCharactersLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class ReplaceSpecialCharacters
+    {
+    }
+}
