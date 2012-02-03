d713bed3f8be2154a7a38121cf159baf3656290c
dusan <rostacik@gmail.com>
2012-02-03 08:13:23 +0100

    deleted portable library project, added full .NET 4.0 project, added tests

---
+++ ReplaceSpecialCharactersLibrary/ReplaceSpecialCharactersLibrary.Tests/ReplaceSpecialCharactersTests.cs
@@ -0,0 +1,121 @@
+﻿namespace ReplaceSpecialCharactersLibrary.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ReplaceSpecialCharactersLibraryFullNET;
+
+    [TestClass]
+    public class ReplaceSpecialCharactersTests
+    {
+        [TestMethod]
+        public void diacritics_will_be_stripped_1()
+        {
+            //arrange
+            string input = "abľščťžýáíé";
+            string output = string.Empty;
+
+            //act
+            var specialChars = new ReplaceSpecialCharacters();
+            output = specialChars.RemoveDiacritics(input);
+
+            //assert
+            Assert.IsNotNull(output);
+            Assert.AreEqual("ablsctzyaie", output);
+        }
+
+        [TestMethod]
+        public void spaces_will_be_replaced_with_default_string_1()
+        {
+            //arrange
+            string input = "abcd efgh";
+            string output = string.Empty;
+
+            //act
+            var specialChars = new ReplaceSpecialCharacters();
+            output = specialChars.ReplaceSpaces(input);
+
+            //assert
+            Assert.IsNotNull(output);
+            Assert.AreEqual("abcd-efgh", output);
+        }
+
+        [TestMethod]
+        public void strip_and_replace_space_1()
+        {
+            //arrange
+            string input = "abľščťžýáíé abľščťžýáíé abľščťžýáíé abľščťžýáíé abcd";
+            string output = string.Empty;
+
+            //act
+            var specialChars = new ReplaceSpecialCharacters();
+            output = specialChars.RemoveDiacriticsAndReplaceSpaces(input, "--");
+
+            //assert
+            Assert.IsNotNull(output);
+            Assert.AreEqual("ablsctzyaie--ablsctzyaie--ablsctzyaie--ablsctzyaie--abcd", output);
+        }
+
+        [TestMethod]
+        public void diacritics_will_be_stripped_and_trimmed_1()
+        {
+            //arrange
+            string input = "    abľščťžýáíé        ";
+            string output = string.Empty;
+
+            //act
+            var specialChars = new ReplaceSpecialCharacters();
+            output = specialChars.TrimAndRemoveDiacritics(input);
+
+            //assert
+            Assert.IsNotNull(output);
+            Assert.AreEqual("ablsctzyaie", output);
+        }
+
+        [TestMethod]
+        public void spaces_will_be_replaced_with_default_string_and_trimmed_1()
+        {
+            //arrange
+            string input = "     abcd efgh         ";
+            string output = string.Empty;
+
+            //act
+            var specialChars = new ReplaceSpecialCharacters();
+            output = specialChars.TrimAndReplaceSpaces(input);
+
+            //assert
+            Assert.IsNotNull(output);
+            Assert.AreEqual("abcd-efgh", output);
+        }
+
+        [TestMethod]
+        public void strip_and_replace_space_and_trim_1()
+        {
+            //arrange
+            string input = "    abľščťžýáíé abľščťžýáíé abľščťžýáíé abľščťžýáíé abcd          ";
+            string output = string.Empty;
+
+            //act
+            var specialChars = new ReplaceSpecialCharacters();
+            output = specialChars.TrimAndRemoveDiacriticsAndReplaceSpaces(input, "--");
+
+            //assert
+            Assert.IsNotNull(output);
+            Assert.AreEqual("ablsctzyaie--ablsctzyaie--ablsctzyaie--ablsctzyaie--abcd", output);
+        }
+
+        [TestMethod]
+        public void strip_and_replace_space_and_trim_2()
+        {
+            //arrange
+            string input = "    abľščťžýáíé abľščťžýáíé abľščťžýáíé abľščťžýáíé abcd          ";
+            string output = string.Empty;
+
+            //act
+            var specialChars = new ReplaceSpecialCharacters();
+            output = specialChars.TrimAndRemoveDiacriticsAndReplaceSpaces(input, null);
+
+            //assert
+            Assert.IsNotNull(output);
+            Assert.AreEqual("    abľščťžýáíé abľščťžýáíé abľščťžýáíé abľščťžýáíé abcd          ", output);
+        }
+    }
+}
---
+++ ReplaceSpecialCharactersLibrary/ReplaceSpecialCharactersLibraryFullNET/ReplaceSpecialCharacters.cs
@@ -0,0 +1,139 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="dusan" file="ReplaceSpecialCharacters.cs">
+//  Feel free to use.
+// </copyright>
+// <summary>
+//   Replaces special characters with their non diacritics counterparts and / or replaces spaces with dash.
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ReplaceSpecialCharactersLibraryFullNET
+{
+    using System.Globalization;
+    using System.Text;
+
+    /// <summary>
+    /// Replaces special characters with their non diacritics counterparts and / or replaces spaces with dash.
+    /// </summary>
+    public class ReplaceSpecialCharacters
+    {
+        /// <summary>
+        /// Removes the diacritics from supplied string.
+        /// </summary>
+        /// <param name="inputString">The input string.</param>
+        /// <returns>
+        /// Input string with removed diacritics.
+        /// </returns>
+        public string RemoveDiacritics(string inputString)
+        {
+            if ((inputString != null) && (!string.IsNullOrWhiteSpace(inputString)))
+            {
+                inputString = inputString.Normalize(NormalizationForm.FormD);
+                var stringBuilder = new StringBuilder();
+
+                for (int i = 0; i < inputString.Length; i++)
+                {
+                    if (CharUnicodeInfo.GetUnicodeCategory(inputString[i]) != UnicodeCategory.NonSpacingMark)
+                    {
+                        stringBuilder.Append(inputString[i]);
+                    }
+                }
+
+                return stringBuilder.ToString();
+            }
+
+            return inputString;
+        }
+
+        /// <summary>
+        /// Replaces the spaces in given string with another string.
+        /// </summary>
+        /// <param name="inputString">The input string.</param>
+        /// <param name="replaceWith">The replace with string.</param>
+        /// <returns>Processed string.</returns>
+        public string ReplaceSpaces(string inputString, string replaceWith = "-")
+        {
+            if ((inputString != null) && (!string.IsNullOrWhiteSpace(inputString)) && (!string.IsNullOrWhiteSpace(replaceWith)))
+            {
+                var stringBuilder = new StringBuilder(inputString);
+                stringBuilder.Replace(" ", replaceWith);
+
+                return stringBuilder.ToString();
+            }
+
+            return inputString;
+        }
+
+        /// <summary>
+        /// Removes the diacritics and replace spaces with supplied string.
+        /// </summary>
+        /// <param name="inputString">The input string to process.</param>
+        /// <param name="replaceWith">The replace with string.</param>
+        /// <returns>Processed string.</returns>
+        public string RemoveDiacriticsAndReplaceSpaces(string inputString, string replaceWith = "-")
+        {
+            var outcome = this.RemoveDiacritics(inputString);
+            outcome = this.ReplaceSpaces(outcome, replaceWith);
+
+            return outcome;
+        }
+
+        /// <summary>
+        /// Trims the and remove diacritics from supplied string.
+        /// </summary>
+        /// <param name="inputString">The input string.</param>
+        /// <returns>Input string with removed diacritics and removed spaces on start and end.</returns>
+        public string TrimAndRemoveDiacritics(string inputString)
+        {
+            if ((inputString != null) && (!string.IsNullOrWhiteSpace(inputString)))
+            {
+                return this.RemoveDiacritics(inputString.Trim());
+            }
+
+            return inputString;
+        }
+
+        /// <summary>
+        /// Trims the and replace spaces in given string with another string.
+        /// </summary>
+        /// <param name="inputString">The input string.</param>
+        /// <param name="replaceWith">The replace with string.</param>
+        /// <returns>Processed string.</returns>
+        public string TrimAndReplaceSpaces(string inputString, string replaceWith = "-")
+        {
+            if ((inputString != null) && (!string.IsNullOrWhiteSpace(inputString)) && (!string.IsNullOrWhiteSpace(replaceWith)))
+            {
+                if (replaceWith != "-")
+                {
+                    return this.ReplaceSpaces(inputString.Trim(), replaceWith);
+                }
+                else
+                {
+                    return this.ReplaceSpaces(inputString.Trim());
+                }
+            }
+
+            return inputString;
+        }
+
+        /// <summary>
+        /// Trims and remove diacritics and replace spaces with supplied string.
+        /// </summary>
+        /// <param name="inputString">The input string.</param>
+        /// <param name="replaceWith">The replace with string.</param>
+        /// <returns>Processed string.</returns>
+        public string TrimAndRemoveDiacriticsAndReplaceSpaces(string inputString, string replaceWith = "-")
+        {
+            if ((inputString != null) && (!string.IsNullOrWhiteSpace(inputString)) && (!string.IsNullOrWhiteSpace(replaceWith)))
+            {
+                var outcome = this.RemoveDiacritics(inputString.Trim());
+                outcome = this.ReplaceSpaces(outcome, replaceWith);
+
+                return outcome;
+            }
+
+            return inputString;
+        }
+    }
+}
